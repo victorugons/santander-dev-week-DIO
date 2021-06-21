b52720d2936eaeeb5ac5edf35e63ce99373dbef2
victorugons <victorugons@gmail.com>
2021-06-20 22:04:17 -0300

    feat: create project initial structure

---
+++ src/main/java/dev/victorugons/santanderdevweek/SantanderDevWeekApplication.java
@@ -0,0 +1,15 @@
+package dev.victorugons.santanderdevweek;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeekApplication.class, args);
+
+	}
+
+}
