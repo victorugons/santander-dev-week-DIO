2632519bdd42b219e6789aac66cb96703ad43f97
victorugons <victorugons@gmail.com>
2021-08-22 00:36:19 -0300

    refactor: moved OpenAPI Bean to another file

--- src/main/java/dev/victorugons/santanderdevweek/SantanderDevWeekApplication.java
+++ src/main/java/dev/victorugons/santanderdevweek/SantanderDevWeekApplication.java
@@ -3,28 +3,20 @@ package dev.victorugons.santanderdevweek;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import lombok.Data;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.*;
 
 @SpringBootApplication
 public class SantanderDevWeekApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SantanderDevWeekApplication.class, args);
-
-	}
-
-	@Bean
-	public OpenAPI customOpenAPI(@Value("${application.description}") String description,
-								 @Value("${application.version}") String version){
-		return new OpenAPI().info(new Info()
-				.title(description)
-				.version(version)
-				.termsOfService("http://swagger.io/terms")
-				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
