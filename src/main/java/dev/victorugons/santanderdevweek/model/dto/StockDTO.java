4d31152f7013004463b54f5e90c40c82b7b0c7f8
victorugons <victorugons@gmail.com>
2021-08-22 00:34:47 -0300

    chore: added examples for the Stock properties to OpenAPI UI

--- src/main/java/dev/victorugons/santanderdevweek/model/dto/StockDTO.java
+++ src/main/java/dev/victorugons/santanderdevweek/model/dto/StockDTO.java
@@ -2,6 +2,7 @@ package dev.victorugons.santanderdevweek.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,20 +16,25 @@ import java.time.LocalDate;
 @Setter
 public class StockDTO {
 
+    @Schema(example = "1")
     private Long id;
 
+    @Schema(example = "StockA")
     @NotNull
     private String name;
 
+    @Schema(example = "10.00")
     @NotNull
     @DecimalMin(value = "0.00")
     @Digits(integer = 6, fraction = 2)
     private Double price;
 
+    @Schema(example = "10.00")
     @NotNull
     @Digits(integer = 3, fraction = 2)
     private Double variation;
 
+    @Schema(example = "01/01/2001")
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate date;
